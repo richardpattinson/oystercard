7976eabbaca3129c0fb9911670c959e4a5400488
Joe Ellingworth <joeellingworth@gmail.com>
2020-01-27 16:10:41 +0000

    step 11 complete

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,26 +1,36 @@
 class Oystercard
-    attr_reader :balance, :en_route
+    attr_reader :balance, :en_route, :entry_station
     MAXIMUM_BALANCE = 90
     MINIMUM_FARE = 1
-    def initialize
-        @balance = 0
-        @en_route = false
+
+    def initialize(balance = 0)
+        @balance = balance
+        @entry_station = nil
     end
+
     def top_up(value)
         fail "Maximum balance of £#{MAXIMUM_BALANCE} exceeded" if (@balance + value) > MAXIMUM_BALANCE
         @balance += value
     end
+
+    def touch_in(station)
+        fail "£#{MINIMUM_FARE} required to touch in" if @balance < MINIMUM_FARE
+        @entry_station = station
+
+    end
+
+    def touch_out
+        deduct(MINIMUM_FARE)
+        @entry_station = nil
+    end
+
+    def in_journey?
+        @entry_station != nil
+    end
+
+    private
+
     def deduct(value)
         @balance -= value
-    end
-    def touch_in
-        fail "£#{MINIMUM_FARE} required to touch in" if @balance < MINIMUM_FARE
-        @en_route = true
-    end
-    def touch_out
-        @en_route = false
-    end
-    def in_journey?
-        @en_route
     end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,42 +1,44 @@
 require "oystercard"
 describe Oystercard do
-    # let(:subject) {Oystercard.new}
+    let(:subject) {Oystercard.new(10)}
+    let(:station) { double :station }
     it "Should have a starting balance of 0" do
-        expect(subject.balance).to eq(0)
+        expect(Oystercard.new.balance).to eq(0)
     end
     describe "#top up" do
         it "Should increase balance by the argument of the top_up method" do
-            expect(subject.top_up(10)).to eq(10)
+            expect(subject.top_up(10)).to eq(20)
         end
         it "Should raise error if card balance would be brought greater than #{Oystercard::MAXIMUM_BALANCE}" do
             expect{ subject.top_up(100) }.to raise_error "Maximum balance of £#{Oystercard::MAXIMUM_BALANCE} exceeded"
         end
     end
-    describe "#deduct" do
-        it "Should be able to deduct from the balance by the argument of the deduct method" do
-            subject.top_up(60)
-            expect(subject.deduct(20)).to eq(40)
-        end
-    end
+
     describe "#touch_in" do
-        it "Should change the en_route instance variable to true" do
-            subject.top_up(10)
-            subject.touch_in
-            expect(subject.en_route).to eq(true)
+        it "Should change the en_route instance variable to true" do 
+            subject.touch_in(station)
+            expect(subject.in_journey?).to eq(true)
         end
         it 'Should raise and error if balance less than £1' do
-            expect { subject.touch_in }.to raise_error "£#{Oystercard::MINIMUM_FARE} required to touch in"
+            expect { Oystercard.new.touch_in(station) }.to raise_error "£#{Oystercard::MINIMUM_FARE} required to touch in"
+        end
+        it "Should remember the name of the touch in station" do
+            subject.touch_in('Angel')
+            expect(subject.entry_station).to eq('Angel')
         end
     end
     describe "#touch_out" do
         it "Should change the en_route instance variable to false" do
             subject.touch_out
-            expect(subject.en_route).to eq(false)
+            expect(subject.in_journey?).to eq(false)
         end
+        it "Should reduce balance by minimum fare" do 
+            expect { subject.touch_out}.to change{subject.balance}.by(-Oystercard::MINIMUM_FARE)
+        end 
     end
     describe "#in_journey?" do 
         it "Should print en_route instance varible" do 
-            expect(subject.in_journey?).to eq(subject.en_route)
+            expect(subject.in_journey?).to eq(subject.in_journey?)
         end
     end
 end
